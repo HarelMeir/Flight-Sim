c1579d2c7f788cfeb5e3dc69683c47911cbf3e5f
SapirHirak <sapiroosh1h@gmail.com>
2021-04-01 18:02:42 +0300

    media player continue

---
+++ controllers/mediaPlayerModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flight_Sim.controllers
+{
+    class mediaPlayerModel
+    {
+        bool play;
+        int speedPlay;
+        double currentTime;
+
+        mediaPlayerModel()
+        {
+
+        }
+    }
+}
