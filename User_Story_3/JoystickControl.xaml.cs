dc312d4e8ea253d46139aeaeac5f6125f72a2ca4
ariel drellich <arieldrellich@gmail.com>
2021-04-04 17:44:14 +0300

    backup before delete

--- User_Story_3/JoystickControl.xaml.cs
+++ User_Story_3/JoystickControl.xaml.cs
@@ -14,19 +14,23 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Flight_Sim.User_Story_3
+
 {
     /// <summary>
     /// Interaction logic for JoystickControl.xaml
     /// </summary>
 
-    
+
     public partial class JoystickControl : UserControl
     {
-        public string ImageName;
+        
+        //JoystickControlViewModel vm;
         public JoystickControl()
         {
-            ImageName = "0 - neutral joystick.png";
             InitializeComponent();
+            //vm = new JoystickControlViewModel(new FlightdataModel());
         }
+
+       
     }
 }
--- User_Story_3/JoystickControlViewModel.cs
+++ User_Story_3/JoystickControlViewModel.cs
@@ -6,34 +6,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
 
 namespace Flight_Sim.User_Story_3
 {
     class JoystickControlViewModel : INotifyPropertyChanged
     {
+        FlightdataModel fdm;
+        public JoystickControlViewModel(FlightdataModel fdm) {
+            this.fdm = fdm;
+        }
+
         public event PropertyChangedEventHandler PropertyChanged = delegate { };
 
         private string imageName = "Joystick_images\\0_neutral_joystick.png";
-        //private string imageName = "Joystick_images\\3_right_joystick.png";
+
         public string ImageName {
             get { return imageName; }
             set {
-                imageName = value;
-                PropertyChanged(this, new PropertyChangedEventArgs(nameof(ImageName)));
+                if (imageName != value)
+                {
+                    imageName = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs(nameof(ImageName)));
+                }
             }
         }
 
-
+        //public const Thickness t 
         private int rudderSliderPos = 400;
         public int RudderSliderPos
         {
             get { return rudderSliderPos; }
             set
             {
-                rudderSliderPos = value;
-                PropertyChanged(this, new PropertyChangedEventArgs(nameof(RudderSliderPos)));
+                if (rudderSliderPos != value)
+                {
+                    rudderSliderPos = value;
+                    //not sure how this works yet so specifically setting throttle and rudder diferently to see which one works.
+                    PropertyChanged(this, new PropertyChangedEventArgs(nameof(RudderSliderPos)));
+                }
             }
         }
+        //private const int rudder0
         public Thickness RudderMargin
         {
             get { return new Thickness(RudderSliderPos, 318, 0, 0); }
@@ -46,52 +62,62 @@ namespace Flight_Sim.User_Story_3
             get { return throttleSliderPos; }
             set
             {
-                throttleSliderPos = value;
-                PropertyChanged(this, new PropertyChangedEventArgs(nameof(ThrottleSliderPos)));
+                if (throttleSliderPos != value)
+                {
+                    throttleSliderPos = value;
+                    //PropertyChanged(this, new PropertyChangedEventArgs(nameof(ThrottleSliderPos)));
+                }
             }
         }
         public Thickness ThrottleMargin
         {
-            get { return new Thickness(244, ThrottleSliderPos, 0, 0); }
+            get {
+                //not sure how this works yet so specifically setting throttle and rudder diferently to see which one works.
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(ThrottleMargin)));
+                return new Thickness(244, ThrottleSliderPos, 0, 0); }
         }
 
+        public void UpdateRudderPosition()
+        {
+            //if RUD < -0.9 -> RudderSliderPos = leftMargin + 1/10 * totalMarginLength
 
+            //
+        }
 
         private double lastAileron = 0;
         private double lastElevator = 0;
-        private int vertical = 0;
-        private int horizontal = 0;
 
         public void UpdateJoystickPosition()
         {
-            
-            //if (lastElevator == currentElevator && lastAileron == currentAileron)
-            //      ImageName = "Joystick_images\\0_neutral_joystick.png";
+            if (lastElevator == this.fdm.Elevator && lastAileron == this.fdm.Aileron)
+                  ImageName = "Joystick_images\\0_neutral_joystick.png";
 
-            //if (lastElevator < currentElevator && lastAileron == currentAileron)
-            //      ImageName = "Joystick_images\\1_up_joystick.png";
+            if (lastElevator < this.fdm.Elevator && lastAileron == this.fdm.Aileron)
+                  ImageName = "Joystick_images\\1_up_joystick.png";
 
-            //if (lastElevator < currentElevator && lastAileron < currentAileron)
-            //      ImageName = "Joystick_images\\2_up-right_joystick.png";
+            if (lastElevator < this.fdm.Elevator && lastAileron < this.fdm.Aileron)
+                  ImageName = "Joystick_images\\2_up-right_joystick.png";
 
-            //if (lastElevator == currentElevator && lastAileron < currentAileron)
-            //      ImageName = "Joystick_images\\3_right_joystick.png";
+            if (lastElevator == this.fdm.Elevator && lastAileron < this.fdm.Aileron)
+                  ImageName = "Joystick_images\\3_right_joystick.png";
 
-            //if (lastElevator > currentElevator && lastAileron < currentAileron)
-            //      ImageName = "Joystick_images\\4_down-right_joystick.png";
+            if (lastElevator > this.fdm.Elevator && lastAileron < this.fdm.Aileron)
+                  ImageName = "Joystick_images\\4_down-right_joystick.png";
 
-            //if (lastElevator > currentElevator && lastAileron == currentAileron)
-            //      ImageName = "Joystick_images\\5_down_joystick.png";
+            if (lastElevator > this.fdm.Elevator && lastAileron == this.fdm.Aileron)
+                  ImageName = "Joystick_images\\5_down_joystick.png";
 
-            //if (lastElevator > currentElevator && lastAileron > currentAileron)
-            //      ImageName = "Joystick_images\\6_down-left_joystick.png";
+            if (lastElevator > this.fdm.Elevator && lastAileron > this.fdm.Aileron)
+                  ImageName = "Joystick_images\\6_down-left_joystick.png";
 
-            //if (lastElevator > currentElevator && lastAileron == currentAileron)
-            //      ImageName = "Joystick_images\\7_left_joystick.png";
+            if (lastElevator > this.fdm.Elevator && lastAileron == this.fdm.Aileron)
+                  ImageName = "Joystick_images\\7_left_joystick.png";
 
-            //if (lastElevator > currentElevator && lastAileron < currentAileron)
-            //      ImageName = "Joystick_images\\8_up-left_joystick.png";
+            if (lastElevator > this.fdm.Elevator && lastAileron < this.fdm.Aileron)
+                  ImageName = "Joystick_images\\8_up-left_joystick.png";
 
+            lastAileron = this.fdm.Aileron;
+            lastElevator = this.fdm.Elevator;
         }
 
     }
