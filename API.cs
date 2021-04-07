e4d36873acd01dabebb40eb1979a2bf0f31c8eb3
Aviv <AvivRahamim95@gmail.com>
2021-04-07 16:09:17 +0300

    added API

---
+++ API.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flight_Sim
+{
+    class API
+    {
+        [DllImport("Dll.dll")]
+        public static extern void uploadCSV(string path);
+        [DllImport("Dll.dll")]
+        public static extern IntPtr createPoint(float x, float y);
+        [DllImport("Dll.dll")]
+        public static extern float getXPoint(IntPtr p);
+        [DllImport("Dll.dll")]
+        public static extern float getYPoint(IntPtr p);
+        [DllImport("Dll.dll")]
+        public static extern string mostCorr(string feature);
+    }
+}
