b3442b90d68aa168b58137add8ef8c70177f83da
Aviv <AvivRahamim95@gmail.com>
2021-04-11 03:23:03 +0300

    changed AnomalyUtils in GraphUtils functions to static, changed Point constructor to floats instead of ints, implemented part of SimpleAnomalyDetector and AnomalyDetector

---
+++ cppToCSharp/AnomalyDetector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flight_Sim.Model;
+
+namespace Flight_Sim.cppToCSharp
+{
+    class AnomalyReport
+    {
+        string description;
+        long timeStep;
+        public AnomalyReport(string description, long timeStep)
+        {
+            this.description = description;
+            this.timeStep = timeStep;
+        }
+        public string desc
+        {
+            get
+            {
+                return description;
+            }
+        }
+        public long ts
+        {
+            get
+            {
+                return timeStep;
+            }
+        }
+    }
+    interface ITimeSeriesAnomalyDetector
+    {
+        void learnNormal(IDictionary<string, List<float>> table);
+        List<AnomalyReport> detect(IDictionary<string, List<float>> table);
+    }
+}
