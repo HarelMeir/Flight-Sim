960ad162e23328feff0b7702283fd5a632438db6
ariel drellich <arieldrellich@gmail.com>
2021-04-11 21:47:22 +0300

    Added learn and all necessary standalone features to CircleAnomalyDetector

--- Squidward's anomaly detector/CircleAnomalyDetector.h
+++ Squidward's anomaly detector/CircleAnomalyDetector.h
@@ -8,14 +8,46 @@
 #ifndef CIRCLEANOMALYDETECTOR_H_
 #define CIRCLEANOMALYDETECTOR_H_
 
-#include "SimpleAnomalyDetector.h"
+#include "anomaly_detection_util.h"
+#include "AnomalyDetector.h"
+#include <vector>
+#include <algorithm>
+#include <string.h>
+#include <math.h>
 #include "minCircle.h"
 
-class CircleAnomalyDetector :public SimpleAnomalyDetector {
+struct correlatedFeatures {
+	string feature1, feature2;  // names of the correlated features
+	float corrlation;
+	Line lin_reg;
+	float threshold;
+	Circle circleThreshold;
+};
+
+class CircleAnomalyDetector :public TimeSeriesAnomalyDetector {
+	float defaultThreshold = 0.9;
+protected:
+	vector<correlatedFeatures> cf;
+	float correlationThreshold = defaultThreshold;
 public:
 	CircleAnomalyDetector();
 	virtual ~CircleAnomalyDetector();
+
+	virtual void learnNormal(const TimeSeries& ts);
 	virtual vector<AnomalyReport> detect(const TimeSeries& ts);
+
+	vector<correlatedFeatures> getNormalModel() {
+		return cf;
+	}
+
+	void setNormalModel(vector<correlatedFeatures> correlatedFeatures);
+
+	float getCorrelationThreshold();
+
+	void setCorrelationThreshold(float newThreshold);
+
+private:
+	float highestDev(float* x, float* y, Line line, int size);
 
 };
 
